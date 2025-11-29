d25bb8e1a2fe74910b2a4d7e479ebdfbbd8ad305
eryel12 <eryel.msy@gmail.com>
2025-11-30 00:38:38 +0700

    Revert "add user page"
    This reverts commit e45e48bbe36434416c57b3c82726c455d1d26576.

--- event-app/src/user.js
+++ event-app/src/user.js
@@ -15,36 +15,9 @@ function User() {
                 </div> 
             </div>
 
-            <div className="container_heading">
-                Welcome, User!
-            </div>
-
             <div className="container_card">
-                <div className="container_card_name">
-                    PPIF
-                </div>
-                <div className="container_card_desc">
-                    Orientasi Mahasiswa Baru
-                </div>
-                <div className="container_card_date">
-                    Date : 26/11/2025
-                </div>
-                <div className="container_card_location">
-                    Location : Lecture Hall
-                </div>
-                <div className="container_card_quota">
-                    <div className="container_card_quota_left">
-                        Quota Left
-                    </div>
-                    <div className="container_card_quota_num">
-                        300
-                    </div>
-                </div>
-                <div className="container_card_press">
-                    <div className="container_card_aplly">
-                        Daftar
-                    </div>
-                </div>
+
+
             </div>
 
         </div>
